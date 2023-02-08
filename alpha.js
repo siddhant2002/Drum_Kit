43c99ab25d0ea5761a8ab614f2612454e2a33f65
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-02-08 11:10:09 +0530

    Files added

--- alpha.js
+++ alpha.js
@@ -1,2 +1,25 @@
-alert("hello")
-window.addEventListener('keydown',function(e))
+
+function chopa(e)
+{
+    var key=document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    var audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio)
+    {
+        return;
+    }
+    audio.currentTime=0;
+    audio.play();
+    key.classList.add('playing'); 
+};
+
+function remove(e)
+{
+    if(e.propertyName!=="transform")
+    {
+        return;
+    }
+    this.classList.remove('playing');
+}
+var keys=document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',remove));
+window.addEventListener('keydown',chopa);
